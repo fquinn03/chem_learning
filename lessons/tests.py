7a51a070da956124b8c4009e1ee3b9dc350649f3
fiona <fionaqui@gmail.com>
2019-07-28 10:08:37 +0100

    separated out assert clauses in tests - only 1 reason to fail

--- lessons/tests.py
+++ lessons/tests.py
@@ -5,20 +5,29 @@ from .models import Lesson
 
 
 class LessonsTest(TestCase):
-    def setUp(cls):
+
+    @classmethod
+    def setUpTestData(cls):
         Level.objects.create(title = "1")
         level = Level.objects.get(id=1)
         Lesson.objects.create(level = level, title = "test_lesson_1", link = "www.testlink1.com", number = 1)
 
+    def setUp(self):
+        self.lesson = Lesson.objects.get(id=1)
+        self.client = Client()
     # models tests
     def test_str(self):
-        lesson = Lesson.objects.get(id=1)
-        self.assertEquals(lesson.__str__(), "test_lesson_1")
+        self.assertEquals(self.lesson.__str__(), "test_lesson_1")
 
     # views tests
-    def test_complete_lesson(self):
-        lesson = Lesson.objects.get(id=1)
-        client = Client()
-        response = client.get(reverse('complete_lesson', args=[1,]))
+    def test_complete_lesson_response(self):
+        response = self.client.get(reverse('complete_lesson', args=[1,]))
         self.assertEquals(response.status_code, 200)
+
+    def test_complete_lesson_template(self):
+        response = self.client.get(reverse('complete_lesson', args=[1,]))
         self.assertTemplateUsed(response, 'lessons/view_lesson.html')
+
+    def test_complete_lesson_html(self):
+        response = self.client.get(reverse('complete_lesson', args=[1,]))
+        self.assertContains(response, "<h5>Lesson Title: test_lesson_1</h5>")
--- teachers/tests.py
+++ teachers/tests.py
@@ -3,22 +3,49 @@ from django.urls import reverse
 from users.models import User
 
 class TeacherTest(TestCase):
-    def setUp(cls):
+
+    @classmethod
+    def setUpTestData(cls):
         User.objects.create(id = 1, username ="teacher", is_student = False, is_teacher = True)
         User.objects.create(id = 2, username ="student", is_student = True, is_teacher = False)
 
-    def test_class_list_teacher(self):
-        teacher = User.objects.get(id=1)
-        student = User.objects.get(id=2)
-        client = Client()
-        response = client.get(reverse('class_list', args=[1] ))
+    def setUp(self):
+        self.client = Client()
+        self.teacher = User.objects.get(id=1)
+        self.student = User.objects.get(id=2)
+
+    def test_is_teacher(self):
+        self.assertEquals(self.teacher.is_teacher, True)
+
+    def test_is_not_teacher(self):
+        self.assertEquals(self.teacher.is_student, False)
+
+    def test_is_not_student(self):
+        self.assertEquals(self.student.is_teacher, False)
+
+    def test_is_student(self):
+        self.assertEquals(self.student.is_student, True)
+
+    def test_class_list_teacher_response(self):
+        response = self.client.get(reverse('class_list', args=[1] ))
         self.assertEquals(response.status_code, 200)
+
+    def test_class_list_teacher_template(self):
+        response = self.client.get(reverse('class_list', args=[1] ))
         self.assertTemplateUsed(response, "teachers/class_list.html")
 
-    def test_class_list_student(self):
-        teacher = User.objects.get(id=1)
-        student = User.objects.get(id=2)
-        client = Client()
-        response = client.get(reverse('class_list', args=[2] ))
+    def test_class_list_teacher_html(self):
+        response = self.client.get(reverse('class_list', args=[1] ))
+        self.assertContains(response, "<h1>Welcome teacher </h1>")
+
+    def test_class_list_student_response(self):
+        response = self.client.get(reverse('class_list', args=[2] ))
         self.assertEquals(response.status_code, 200)
+
+    def test_class_list_student_template(self):
+        response = self.client.get(reverse('class_list', args=[2] ))
         self.assertTemplateUsed(response, "teachers/not_teacher.html")
+
+    def test_class_list_student_html(self):
+        response = self.client.get(reverse('class_list', args=[2] ))
+        self.assertContains(response, "<p>You do teach any classes</p>")
