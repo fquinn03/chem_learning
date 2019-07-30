aed67accf3d3ba97745afff0fe67f46a3758fb00
fiona <fionaqui@gmail.com>
2019-07-30 14:01:27 +0100

    added modal to display chemical formula

--- teachers/static/js/javascriptsource.js
+++ teachers/static/js/javascriptsource.js
@@ -1,4 +1,11 @@
-$( document ).ready(function() {
-  $('.mymodal').modal();
-  $('.modal-content').load("{% url 'get_formula' %}");
-});
+$(document).ready(function(){
+
+  $('#convert').click(function() {
+    var output = document.getElementById("formula_output")
+    output.innerHTML=""
+    var input = document.getElementById('chemformula')
+    raw_formula = input.value;
+    output.innerHTML += "<p>$$\\ce{"+ raw_formula +"}$$</p>"
+    MathJax.Hub.Queue(["Typeset",MathJax.Hub,"formula_output"]);
+  })
+})
