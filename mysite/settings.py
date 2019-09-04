d997bb3b5bcabca81b316c8290159b8a0abfd16d
fiona <fionaqui@gmail.com>
2019-09-04 16:52:11 +0100

    changes to static

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,9 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')+h8cgd&kr67q4%)t%r(n!gki_7sr5!e_*@!*6ose3psg19#v8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['chemlearning.pythonanywhere.com',]
+ALLOWED_HOSTS = ['127.0.0.1',]
+
 
 # Application definition
 
@@ -73,15 +74,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = { 'default':
-            { 'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'chemlearning$ChemLearning',
-            'USER': 'chemlearning',
-            'PASSWORD': 'TingFRLJ7tL8',
-            'HOST': 'chemlearning.mysql.pythonanywhere-services.com',
-            'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8mb4',}}}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
@@ -122,6 +120,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-LOGIN_URL = 'home'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
